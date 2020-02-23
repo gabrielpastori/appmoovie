6f91d1f3e24d4a090723f6e57c76fd0b4990041e
gabrielpastori <gabrielfigueira@alunos.utfpr.edu.br>
2020-02-23 20:26:10 -0300

    Add Form&Chart

---
+++ js/chart.js
@@ -0,0 +1,197 @@
+var allMovieInfos=[];
+
+$.ajax({
+  method: "GET",
+  dataType: "json",
+  url: "https://api.themoviedb.org/3/movie/top_rated?api_key="+KEY+"&language="+LANGUAGE+'&region='+REGION+"&page=1",
+  async: false, 
+  success: function(response) {
+    var results = response.results;
+  
+    for(i=0;i<results.length;i++){
+        var movieObj={};
+        movieObj['vote_count']=results[i].vote_count;
+        movieObj['title']=formatLabel(results[i].title,22);
+        allMovieInfos.push(movieObj)
+    }
+  }
+});
+/*$.getJSON("https://api.themoviedb.org/3/movie/top_rated?api_key="+KEY+"&language="+LANGUAGE+'&region='+REGION+"&page=1").then(function(response){
+  console.log(response);
+  var results = response.results;
+  
+  for(i=0;i<results.length;i++){
+      var movieObj={};
+      movieObj['vote_count']=results[i].vote_count;
+      movieObj['title']=formatLabel(results[i].title,22);
+      allMovieInfos.push(movieObj)
+  }
+});*/
+
+function formatLabel(str, maxwidth){
+  var sections = [];
+  var words = str.split(" ");
+  var temp = "";
+
+  words.forEach(function(item, index){
+      if(temp.length > 0)
+      {
+          var concat = temp + ' ' + item;
+
+          if(concat.length > maxwidth){
+              sections.push(temp);
+              temp = "";
+          }
+          else{
+              if(index == (words.length-1))
+              {
+                  sections.push(concat);
+                  return;
+              }
+              else{
+                  temp = concat;
+                  return;
+              }
+          }
+      }
+
+      if(index == (words.length-1))
+      {
+          sections.push(item);
+          return;
+      }
+
+      if(item.length < maxwidth) {
+          temp = item;
+      }
+      else {
+          sections.push(item);
+      }
+
+  });
+
+  return sections;
+}
+
+function extractMovieInfos(arr){
+
+  let labels=[]
+  let data=[]
+  for(var i=0;i<Math.min(6,arr.length);i++){
+    labels.push(arr[i].title);
+    data.push(arr[i].vote_count);
+  }
+  return [labels,data];
+}
+var movieInfos=extractMovieInfos(allMovieInfos);
+var movieLabels=movieInfos[0];
+var movieData=movieInfos[1];
+var horizontalChart = new Chart(document.getElementById("bar-chart-horizontal"), {
+  type: 'horizontalBar',
+  data: {
+    labels: movieLabels,
+    datasets: [
+      {
+        label: "Votos",
+        backgroundColor: "#3e95cd",
+        data: movieData
+      }
+    ]
+  },
+  options: {
+    responsive:true,
+    maintainAspectRatio:true,
+    legend: { display: false },
+    scales:{
+      yAxes: [{
+          display: false 
+      }],
+      xAxes: [{
+        display: true,
+        ticks: {
+          min: 0
+        } 
+      }]
+    },
+    title: {
+      display: false,
+      text: 'Predicted world population (millions) in 2050'
+    }
+  }
+});
+
+$("#inputGroupSelect01").change(function(){
+  var selectedYear = $(this).children("option:selected").val();
+  if(selectedYear=="Qualquer Ano"){
+    allMovieInfos=[];
+    $.ajax({
+      method: "GET",
+      dataType: "json",
+      url: "https://api.themoviedb.org/3/movie/top_rated?api_key="+KEY+"&language="+LANGUAGE+'&region='+REGION+"&page=1",
+      async: false, 
+      success: function(response) {
+        var results = response.results;
+        console.log(results)
+        for(i=0;i<results.length;i++){
+            var movieObj={};
+            movieObj['vote_count']=results[i].vote_count;
+            movieObj['title']=formatLabel(results[i].title,22);
+            allMovieInfos.push(movieObj)
+        }
+        var movieInfos=extractMovieInfos(allMovieInfos);
+        var movieLabels=movieInfos[0];
+        var movieData=movieInfos[1];
+        horizontalChart.destroy();
+        horizontalChart = new Chart(document.getElementById("bar-chart-horizontal"), { type: 'horizontalBar', data: { labels: movieLabels, datasets: [ { label: "Votos", backgroundColor: "#3e95cd", data: movieData } ] }, options: { responsive:true, maintainAspectRatio:true, legend: { display: false }, scales:{ yAxes: [{ display: false }], xAxes: [{ display: true, ticks: { min: 0 } }] }, title: { display: false, text: 'Predicted world population (millions) in 2050' } }});
+
+      }
+    });
+    /*
+    var labels=['a','b','c','d','e','f'];
+    var dataS=[1,2,3,4,5,6];
+    horizontalChart.destroy();
+    horizontalChart = new Chart(document.getElementById("bar-chart-horizontal"), { type: 'horizontalBar', data: { labels: labels, datasets: [ { label: "Votos", backgroundColor: "#3e95cd", data: dataS } ] }, options: { responsive:true, maintainAspectRatio:true, legend: { display: false }, scales:{ yAxes: [{ display: false }], xAxes: [{ display: false, ticks: { min: 0 } }] }, title: { display: false, text: 'Predicted world population (millions) in 2050' } }});*/
+  }else{
+    allMovieInfos=[];
+    $.ajax({
+      method: "GET",
+      dataType: "json",
+      url: "https://api.themoviedb.org/3/movie/top_rated?api_key=6f228a124b52956ac305a349079b7f2b&language=pt-BR&region=BR&page=1&primary_release_year="+selectedYear,
+      async: false, 
+      success: function(response) {
+        var results = response.results;
+        for(i=0;i<Math.min(results.length,6);i++){
+            var movieObj={};
+            movieObj['vote_count']=results[i].vote_count;
+            movieObj['title']=formatLabel(results[i].title,22);
+            allMovieInfos.push(movieObj)
+        }
+        if($("#inputGroupSelect02 option:selected").text()=="Mais votado"){
+          allMovieInfos.sort(function(a,b){
+            if(a.vote_count<b.vote_count) return 1;
+            if(a.vote_count>b.vote_count) return -1;
+            return 0;
+          })
+        }else{
+          allMovieInfos.sort(function(a,b){
+            if(a.vote_count<b.vote_count) return -1;
+            if(a.vote_count>b.vote_count) return 1;
+            return 0;
+          })
+
+        }
+        var movieInfos=extractMovieInfos(allMovieInfos);
+        var movieLabels=movieInfos[0];
+        var movieData=movieInfos[1];
+        horizontalChart.destroy();
+        
+        horizontalChart = new Chart(document.getElementById("bar-chart-horizontal"), { type: 'horizontalBar', data: { labels: movieLabels, datasets: [ { label: "Votos", backgroundColor: "#3e95cd", data: movieData } ] }, options: { responsive:true, maintainAspectRatio:true, legend: { display: false }, scales:{ yAxes: [{ display: false }], xAxes: [{ display: true, ticks: { min: 0 } }] }, title: { display: false, text: 'Predicted world population (millions) in 2050' } }});
+
+      }
+    });
+  }
+});
+$("#inputGroupSelect02").change(function(){
+  $("#inputGroupSelect01").trigger("change");
+
+})
--- js/main.js
+++ js/main.js
@@ -1,36 +1,80 @@
 const KEY="6f228a124b52956ac305a349079b7f2b"
 const LANGUAGE = 'pt-BR';
 const REGION = 'BR';
-function nowPlaying(){
-    var movieIds = [];
-    $.getJSON('https://api.themoviedb.org/3/movie/now_playing?api_key='+KEY+'&language='+LANGUAGE+'&region='+REGION).then(function(response){
-        console.log(response)
-        for(var i=0;i<3;i++){
-        
-            var eachResult=response.results[i];
-            movieIds.push(eachResult.id);
-            $($(".slide-image")[i]).attr("src","https://image.tmdb.org/t/p/original"+eachResult.backdrop_path)
-            $($(".movie-info h4")[i]).text(eachResult.title);
-        }
-        console.log(movieIds)
-        movieIds.forEach(function(id,index){
-            $.getJSON("https://api.themoviedb.org/3/movie/"+id+"?api_key="+KEY).then(function(responseForId){
-                $($(".movie-duration")[index]).text("Duração: "+responseForId.runtime+" minutos.");
-                var genres="";
-                var responseGenresLenght = responseForId.genres.length;
-                responseForId.genres.forEach(function(val,idx){
-                    genres+=val.name;
-                    
-                    if(idx==(responseGenresLenght-1)) genres+=".";
-                    else genres+=", "
-                    
-                })
-                $($(".movie-genres")[index]).text(genres);
-            });
-        })
+$(window).on("load",function(){
+    function nowPlaying(){
+        var movieIds = [];
+        $.getJSON('https://api.themoviedb.org/3/movie/now_playing?api_key='+KEY+'&language='+LANGUAGE+'&region='+REGION).then(function(response){
+            console.log(response)
+            for(var i=0;i<3;i++){
             
+                var eachResult=response.results[i];
+                movieIds.push(eachResult.id);
+                $($(".slide-image")[i]).attr("src","https://image.tmdb.org/t/p/original"+eachResult.backdrop_path)
+                $($(".movie-info h4")[i]).text(eachResult.title);
+            }
+            console.log(movieIds)
+            movieIds.forEach(function(id,index){
+                $.getJSON("https://api.themoviedb.org/3/movie/"+id+"?api_key="+KEY+'&language='+LANGUAGE).then(function(responseForId){
+                    $($(".movie-duration")[index]).text("Duração: "+responseForId.runtime+" minutos.");
+                    var genres="";
+                    var responseGenresLenght = responseForId.genres.length;
+                    responseForId.genres.forEach(function(val,idx){
+                        genres+=val.name;
+                        
+                        if(idx==(responseGenresLenght-1)) genres+=".";
+                        else genres+=", "
+                        
+                    })
+                    $($(".movie-genres")[index]).text(genres);
+                });
+            })
+                
+        });
+        
+        
+    }
+    nowPlaying();
+    function drawMovies(arr){
+        $("#search-result").empty();
+        var limit=6;
+        for(var i=0;i<limit;i++){
+            if(arr[i].poster_path==null){
+                limit++;
+            }else{
+                $("#search-result").append($("<img>").attr("src","https://image.tmdb.org/t/p/original"+arr[i].poster_path));
+            }
+        }
+    }
+    $(window).on("scroll",function(){
+        if($(window).scrollTop()>10){
+            $(".navbar")
+            .removeClass("navbar-dark bg-transparent")
+            .addClass("navbar-light bg-light")
+            
+        
+        }else{
+            $(".navbar")
+            .addClass("navbar-dark bg-transparent")
+            .removeClass("navbar-light bg-light");
+        }
     });
+    $("#movie-search").keyup(function(){
+        if($(this).val()==""){
+            $("#search-result").empty();
+
+
+            console.log("aaa");
+        }else{
+            $.getJSON("https://api.themoviedb.org/3/search/movie?api_key="+KEY+"&language="+LANGUAGE+"&query="+$(this).val()).then(function(response){
+                arr = response.results;
+                drawMovies(arr); 
+            });
+        }
+    });
+
+    
+
     
     
-}
-nowPlaying();
+});
