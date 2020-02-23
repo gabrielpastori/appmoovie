323a2895be3421343087eb8bb989b4e9c20430ba
gabrielpastori <gabrielfigueira@alunos.utfpr.edu.br>
2020-02-23 02:39:08 -0300

    Add movie infos

--- js/main.js
+++ js/main.js
@@ -2,14 +2,35 @@ const KEY="6f228a124b52956ac305a349079b7f2b"
 const LANGUAGE = 'pt-BR';
 const REGION = 'BR';
 function nowPlaying(){
+    var movieIds = [];
     $.getJSON('https://api.themoviedb.org/3/movie/now_playing?api_key='+KEY+'&language='+LANGUAGE+'&region='+REGION).then(function(response){
-        console.log(response.results[0].backdrop_path)
+        console.log(response)
         for(var i=0;i<3;i++){
-            $($(".slide-image")[i]).attr("src","https://image.tmdb.org/t/p/original"+response.results[i].backdrop_path)
-
+        
+            var eachResult=response.results[i];
+            movieIds.push(eachResult.id);
+            $($(".slide-image")[i]).attr("src","https://image.tmdb.org/t/p/original"+eachResult.backdrop_path)
+            $($(".movie-info h4")[i]).text(eachResult.title);
         }
-
+        console.log(movieIds)
+        movieIds.forEach(function(id,index){
+            $.getJSON("https://api.themoviedb.org/3/movie/"+id+"?api_key="+KEY).then(function(responseForId){
+                $($(".movie-duration")[index]).text("Duração: "+responseForId.runtime+" minutos.");
+                var genres="";
+                var responseGenresLenght = responseForId.genres.length;
+                responseForId.genres.forEach(function(val,idx){
+                    genres+=val.name;
+                    
+                    if(idx==(responseGenresLenght-1)) genres+=".";
+                    else genres+=", "
+                    
+                })
+                $($(".movie-genres")[index]).text(genres);
+            });
+        })
             
     });
+    
+    
 }
 nowPlaying();
