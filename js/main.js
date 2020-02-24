da3dd446fac8cd1c506ce99dbad70c3f07bd2778
gabrielpastori <gabrielfigueira@alunos.utfpr.edu.br>
2020-02-24 03:36:19 -0300

    Add Search&Modal

--- js/main.js
+++ js/main.js
@@ -35,17 +35,35 @@ $(window).on("load",function(){
     nowPlaying();
     function drawMovies(arr){
         $("#search-result").empty();
+        $("#search-title").remove();
+        $("<h3 id='search-title'>Resultados da busca</h3>").insertBefore("#search-result");
         var limit=6;
         for(var i=0;i<limit;i++){
             if(arr[i].poster_path==null){
                 limit++;
             }else{
-                
                 var imageUrl = "https://image.tmdb.org/t/p/original"+arr[i].poster_path;
-                var routerView = "<a href='#/sobre' class='router-img'><img src="+imageUrl+" style=></img></a>";
-                $("#search-result").append(routerView);
+                var img = "<img src="+imageUrl+" class='movie-poster' id="+arr[i].id+"></img>";
+                $("#search-result").append(img);
             }
         }
+    }
+    function formatDate(date) {
+        var monthNames = [
+          "Janeiro", "Fevereiro", "Março",
+          "Abril", "Maio", "Junho", "Julho",
+          "Agosto", "Setembro", "Outubro",
+          "Novembro", "Dezembro"
+        ];
+      
+        var day = date.getDate();
+        var monthIndex = date.getMonth();
+        var year = date.getFullYear();
+      
+        return day + ' de ' + monthNames[monthIndex] + ', ' + year;
+    }
+    function formatMoney(number) {
+        return number.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
     }
     $(window).on("scroll",function(){
         if($(window).scrollTop()>10){
@@ -63,6 +81,7 @@ $(window).on("load",function(){
     $("#movie-search").keyup(function(){
         if($(this).val()==""){
             $("#search-result").empty();
+            $("#search-title").remove();
         }else{
             $.getJSON("https://api.themoviedb.org/3/search/movie?api_key="+KEY+"&language="+LANGUAGE+"&query="+$(this).val()).then(function(response){
                 arr = response.results;
@@ -71,7 +90,40 @@ $(window).on("load",function(){
         }
     });
     
-    
+    $("#search-result").on("click","img",function(){
+        $('#movieModal').modal('show');
+        var movieId=$(this).attr("id");
+        $.getJSON("https://api.themoviedb.org/3/movie/"+movieId+"?api_key="+KEY+'&language='+LANGUAGE).then(function(response){
+            console.log(response);
+            var imageUrl = "https://image.tmdb.org/t/p/original"+response.backdrop_path;
+            $(".modal-title").text(response.title);
+            $("#modal-poster").attr("src",imageUrl);
+            $("#movie-overview").text(response.overview);
+            $("#release-date").text("Lançamento: "+formatDate(new Date(response.release_date)));
+            $("#movie-revenue").text("Receita: "+formatMoney(response.revenue));
+            $("#movie-budget").text("Orçamento: "+formatMoney(response.budget));
+            $(".progress-bar").css("width",response.vote_average*10+"%");
+            $(".progress-bar").text("Avaliação média: "+response.vote_average);
+        })
+        $.getJSON("https://api.themoviedb.org/3/movie/"+movieId+"/credits?api_key="+KEY+'&language='+LANGUAGE).then(function(response){
+            var filteredCast=response.cast.slice(0,Math.min(response.cast.length,6));
+            var movieCast="";
+            filteredCast.forEach(function(val,idx){
+                movieCast+=val.character;
+                
+                if(idx==(Math.min(response.cast.length,6)-1)) movieCast+=".";
+                else movieCast+=", "
+                
+            })
+            $("#movie-cast").text("Elenco: "+movieCast);
+            
+            
+        })
+
+        
+
+
+    })
 
     
     
